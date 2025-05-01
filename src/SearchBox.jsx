5772f0778672de1339f46904d759413071446e4b
Sunidhi chauhan <149896712+sunidhi3@users.noreply.github.com>
2025-05-01 12:51:26 +0530

    Update SearchBox.jsx
    update

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -7,7 +7,7 @@ export default function SearchBox({updateInfo}){
     let [city,setCity]=useState("");
     let [error,setError]=useState(false);
 
-    //const API_URL="https://api.openweathermap.org/data/2.5/weather";
+    
    
 
     let getWeatherInfo=async()=>{
